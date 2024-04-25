478f47221dd4c66476b1d335ffded196e16c42c9
Amalekh-Biju <bamalekh@gmail.com>
2024-04-25 18:06:55 +0530

    Margin FIx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,14 +5,6 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="scroll-watcher"></div>
-      <div className="ac">
-        <br /><br /><br />
-        <div className="footer-content">
-          <h4>Designed and Managed</h4>
-          <h2>❤</h2>
-          <a href="https://ac-inc.in" rel="noreferrer" target='_blank'><h1>A C</h1></a>
-        </div>
-      </div>
     </div>
   )
 }
